8879cb5b892a11d9098c8d87df3199976df910f3
ymotongpoo <ymotongpoo AT gmail.com>
2011-09-08 08:48:23 +0900

    implemented urlread function

--- py2ch_0003/py2ch/utility.py
+++ py2ch_0003/py2ch/utility.py
@@ -23,7 +23,7 @@ config = {'url_pattern': r'http://<server>/<board>/dat/<id>.dat',
 追加取得をする場合gzipを付与してはいけないことを確認
 http://www.monazilla.org/index.php?e=198
 """
-httpheaders = {'User-Agent': 'Monazilla/1.00 (py2ch/0.10)'
+httpheaders = {'User-Agent': 'Monazilla/1.00 (py2ch/0.10)',
                'Accept-Encoding': 'gzip',
                'Accept-Language': 'ja',
                'If-Modified-Since': None,
@@ -41,30 +41,18 @@ def get_root_path():
 
 
 def urlread(url, headers):
-  req = urllib2.Request(url, headers)
+  req = urllib2.Request(url, headers=headers)
   p = urllib2.urlopen(req)
   data = p.read()
+  info = p.info()
   if p.info().get('Content-Encoding') in ('gzip', 'x-gzip'):
     s = StringIO(data)
     z = gzip.GzipFile('rb', fileobj=s)
     data = z.read()
-  return data
+  return (info, data)
 
 
-
-
-
-
-class ThreadManager(_MetaThreadManagerObject):
-  def __init__(self, config, httpheaders):
-    url_pattern = config['url_pattern']
-    encoding = config['encoding']
-    separator = config['separator']
-    past_url_patterns = config['past_url_patterns']
-    _MetaThreadObject(url_pattern, encoding, seaprator, past_url_patterns)
-
-
-class _MetaThreadManagerObject(object):
+class _MetaThreadManager(object):
   def __init__(self, url_pattern, encoding, separator, past_url_patterns):
     self.url_pattern = url_pattern
     self.encoding = encoding
@@ -73,5 +61,37 @@ class _MetaThreadManagerObject(object):
     self.id = None
 
 
+class ThreadManager(_MetaThreadManager):
+  def __init__(self, config, httpheaders):
+    url_pattern = config['url_pattern']
+    encoding = config['encoding']
+    separator = config['separator']
+    past_url_patterns = config['past_url_patterns']
+    _MetaThreadManager.__init__(self, url_pattern, encoding, 
+                                separator, past_url_patterns)
+
+    self.httpheaders = httpheaders
+    self.threads = set()
+
+  def register_threads(self, threads):
+    for t in threads:
+      self.threads.add(t)
+
+  def fetch_thread_data(self, url):
+    info, data = urlread(url, self.httpheaders)
+    return info, data.decode(self.encoding, 'ignore')
+
+
 if __name__ == '__main__':
-  print get_root_path()
+  threads = ['http://kamome.2ch.net/traf/dat/1310723624.dat',
+             'http://kamome.2ch.net/traf/dat/1313507415.dat',
+             'http://yuzuru.2ch.net/ftax/dat/1223984982.dat',
+             'http://yuzuru.2ch.net/ftax/dat/1196851586.dat']
+  tm = ThreadManager(config, httpheaders)
+  tm.register_threads(threads)
+
+  for t in tm.threads:
+    print "*"*10 + t
+    info, data = tm.fetch_thread_data(t)
+    print data
+
