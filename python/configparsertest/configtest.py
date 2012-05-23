45b3d098620a0270c8bf6fe3e0902ff5f2ff751b
ymotongpoo <ymotongpoo AT gmail.com>
2012-05-23 21:59:23 +0900

    added ConfigParser sample

---
+++ python/configparsertest/configtest.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+import ConfigParser
+
+config = ConfigParser.SafeConfigParser()
+config.read("test.cfg")
+
+print config.get('test1', 'property1')
+print config.get('test1', 'property2')
+print config.get('test1', 'property3')
+
+print config.getint('test2', 'foo')
+print config.getfloat('test2', 'bar')
+print config.getboolean('test2', 'buz')
+
